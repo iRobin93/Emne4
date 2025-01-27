ac2478bc6876fff4df2abbcd7dd7884bdf726b0c
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2025-01-27 11:30:53 +0100

    x

---
+++ Backend/WeatherDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Backend
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<WeatherForecast> forecast { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+    }
+}
