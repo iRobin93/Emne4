6222ec578e88f3e5a977a3b80b2c35eb6e9099c5
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2025-01-28 14:15:36 +0100

    x

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -10,13 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("*") // Allow the specific origin
+              .AllowAnyHeader()                     // Allow any header
+              .AllowAnyMethod();                     // Allow any HTTP method (including DELETE)
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+// Enable CORS middleware
+app.UseCors("AllowSpecificOrigin");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
