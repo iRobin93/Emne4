88b9ab65bc09a28cd43147d7557c20f547b2ec00
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2025-01-29 11:36:44 +0100

    x

--- BackendNodeJS/index.mjs
+++ BackendNodeJS/index.mjs
@@ -8,7 +8,7 @@ import cors from 'cors';
 import sql from 'mssql';
 import https from 'https';
 import fs from 'fs';
-import {format} from 'date-fns'
+import { format } from 'date-fns'
 
 
 class WeatherForecast {
@@ -74,28 +74,46 @@ async function queryDatabase() {
 }
 
 
+
 // Create an Express application
 const app = express();
+app.use(express.json());
 app.use(cors());
 // Define a route
 app.get('/WeatherForecast', async (req, res) => {
   // res.send('Hello, World!');
-  try{
+  try {
     await sql.connect(config)
     const result = await sql.query('SELECT * FROM forecast');
     const forecasts = result.recordset.map(f => new WeatherForecast(f.date, f.temperatureC, f.summary));
     res.json(forecasts);
   }
-  catch(err){
+  catch (err) {
     console.error('Error connecting to the database:', err);
     res.status(500).send('Error fetching data');
   }
-  finally{
+  finally {
     await sql.close();
   }
 });
 
+app.put('/WeatherForecast', async (req, res) => {
+  try {
+    await sql.connect(config)
+    const forecast = req.body;
+    const result = await sql.query`UPDATE forecast SET summary = ${forecast.summary}, temperatureC = ${forecast.temperatureC} WHERE date = ${forecast.date}`;
+    console.log('Update successful', result);
+  }
+  catch (err) {
+    console.error('Error updating record:', err);
+    res.status(500).send('Error putting data');
+  } finally {
+    // Close the connection
+    await sql.close();
+  }
 
+
+});
 
 // Start the server
 const PORT = 7175;
