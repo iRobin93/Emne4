5d0dae42f47996828972cd3550d741d92a78177f
iRobin93 <robin_ms_3@hotmail.com>
2025-02-10 14:41:01 +0100

    Updates

--- musicapp/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Emne4/musicapp/'  // replace with your repo name (case-sensitive)
-    : '/'
-}
