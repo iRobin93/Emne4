33f00dd807ed0d1f7f5e72cd9c02cc73db1408fb
iRobin93 <robin_ms_3@hotmail.com>
2025-02-10 14:19:43 +0100

    Update vue.config.js

--- musicapp/vue.config.js
+++ musicapp/vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/musicapp/'  // replace with your repo name (case-sensitive)
+    ? '/Emne4/'  // replace with your repo name (case-sensitive)
     : '/'
 }
